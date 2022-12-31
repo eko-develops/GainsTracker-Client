1148183176354ffa40d2dc1d2a60793d552b25f6
eko-develops <ekodevelops@gmail.com>
2022-12-31 15:08:11 -0800

    show pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Exercises from './pages/Exercises';
+import Home from './pages/Home';
+import Workouts from './pages/Workouts';
 
 function App() {
 	return (
 		<div className="app-container">
 			<Header />
 			<h1>Hello World</h1>
+			<Home />
+			<Exercises />
+			<Workouts />
 			<Footer />
 		</div>
 	);
