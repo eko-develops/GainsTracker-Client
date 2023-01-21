cbb0a7b9cdaecf492c45da5db31b20224e6a34b5
eko-develops <ekodevelops@gmail.com>
2023-01-20 20:44:44 -0800

    add links for react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import Header from './components/Header';
 import Exercises from './pages/Exercises';
 import Home from './pages/Home';
 import Workouts from './pages/Workouts';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="app-container">
 			<Header />
-			<h1>Hello World</h1>
-			<Home />
-			<Exercises />
-			<Workouts />
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/exercises" element={<Exercises />} />
+					<Route path="/workouts" element={<Workouts />} />
+				</Routes>
+			</main>
 			<Footer />
 		</div>
 	);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
 function Header() {
-	return <header>Header</header>;
+	return (
+		<header>
+			<div className="brand">brand</div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/exercises">Exercises</Link>
+					</li>
+					<li>
+						<Link to="/workouts">Workouts</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
