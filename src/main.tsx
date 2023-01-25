6e4cc48d01d843ee3306374933c216ab50a39ecc
eko-develops <ekodevelops@gmail.com>
2023-01-24 16:40:55 -0800

    remove react router and move bootstrap css import to bootstrap.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<App />
 	</React.StrictMode>
 );
