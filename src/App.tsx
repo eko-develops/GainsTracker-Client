1bfe47af1bb7f7ae36b2c42fde1791dddebe324f
eko-develops <ekodevelops@gmail.com>
2023-01-24 16:39:50 -0800

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-
 import './App.scss';
-
-import Home from './pages/Home';
-import Exercises from './pages/Exercises';
-import Workouts from './pages/Workouts';
-
-import Header from './components/Header';
-import { Container } from 'react-bootstrap';
-import OffCanvas from './components/OffCanvas';
 
 function App() {
 	return (
-		<>
-			{/* <Header /> */}
-			<OffCanvas />
-			<Container as="main">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/exercises" element={<Exercises />} />
-					<Route path="/workouts" element={<Workouts />} />
-				</Routes>
-			</Container>
-		</>
+		<div className="min-vh-100 bg-secondary">
+			<h1 className=" text-primary">Hello World</h1>
+			<h2 className="text-primary">Hello World again</h2>
+			<h3 className="text-secondary bg-primary">Hello World again, again</h3>
+		</div>
 	);
 }
 
